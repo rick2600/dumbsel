b050b9d12170c60b4c50d42bccaa099697b05e99
rick <ricks2600@gmail.com>
2015-02-24 14:30:18 -0300

    fixing leak

--- main.c
+++ main.c
@@ -1,22 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
 #include "vm.h"
 
+vm_t *vm;
+
+void handler(int sig);
 
 int main(int argc, char **argv)
 {
 
-  vm_t *vm;
+  vm = NULL;
 
   if (argc != 2)
   {
     printf("usage: %s <code.bin>\n", argv[0]);
     exit(EXIT_FAILURE);
   }
+  signal(SIGINT, handler);
   vm = create_vm();
 
   turn_on(vm, argv[1]);
   turn_off(vm);
   
   exit(EXIT_SUCCESS);
+}
+
+
+void handler(int sig)
+{  
+  if (vm && vm->cpu)
+  {
+    printf("\npress enter to exit\n");
+    vm->cpu->halt = 1;
+  }
 }
